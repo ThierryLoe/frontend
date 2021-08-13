fcfaa762ce1dd993867c477d675e39a9eb02c13a
Thierry <thierry.loetscher@komaxgroup.com>
2021-08-13 15:31:49 +0200

    added Users View

---
+++ src/components/AllUsers.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <h1>Users</h1>
+      <ul>
+          <li v-for="user in users" v-bind:key="user.personID">{{user.lastName}}{{user.firstName}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'users',
+    data() {
+        return{
+            users: []
+        }
+    },
+    created: async function(){
+        let res = await axios.get("http://localhost:7071/api/user")
+        this.users = res.data
+    }
+}
+</script>
+
+<style>
+
+</style>
+
